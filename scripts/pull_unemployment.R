10842b8753bf4b63a6e75de17de52598aa5d6cc3
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2019-01-25 19:03:59 -0600

    collated graphs in Rmd; worked on new epop graph

--- scripts/pull_unemployment.R
+++ scripts/pull_unemployment.R
@@ -12,15 +12,20 @@ source("scripts/setup.R")
 
 cps_codes <- fread("https://download.bls.gov/pub/time.series/ln/ln.series")
 
+epop <- filter(cps_codes, str_detect(series_title, "Employment-Population"),
+               periodicity_code == "M", sexs_code ==0 )
+
 # construct query
 cps_query <- filter(cps_codes, periodicity_code == "M",
-                    substr(series_id, 4, 10) %in% c("1230000"),
+                    substr(series_id, 4, 12) %in% c("02300060", "02300063",
+                                                    "02300066", "02300069"),
                     sexs_code == 0)
+
 
 year_intervals <- sapply(seq(1954, 2018, 19), function(x) seq(x, x + 18, 18))
 year_intervals[2,4] <- 2018
 
-unemp_raceth_data <- map2(year_intervals[1, ] , year_intervals[2, ],
+raceth_data <- map2(year_intervals[1, ] , year_intervals[2, ],
   function(x, y) bls_api(cps_query$series_id, startyear = x, endyear = y,
                      registrationKey = blskey)) %>% bind_rows() %>% 
   dplyr::rename(series_id = seriesID) %>%
@@ -32,19 +37,19 @@ unemp_raceth_data <- map2(year_intervals[1, ] , year_intervals[2, ],
            substr(series_id, 11, 11) == "9" ~ "hispanic"
          ))
 
-unemp_all <- unemp_raceth_data %>% filter(substr(series_id, 11, 11) == "0") %>% 
-  dplyr::rename(unemp_all = value) %>% 
-  select(month, unemp_all)
+epop_all <- raceth_data %>% filter(substr(series_id, 11, 11) == "0") %>% 
+  dplyr::rename(epop_all = value) %>% 
+  select(month, epop_all)
 
-unemp_raceth <- unemp_raceth_data %>% 
+epop_raceth <- raceth_data %>% 
   filter(raceth %in% c("white", "black")) %>% select(-series_id) %>% 
   spread(raceth, value) %>% 
   mutate(bw_delta = black - white) %>% 
   left_join(unemp_all, by = "month")
 
-ggplot(unemp_raceth, aes(x = white, y = black, color = year)) +
+ggplot(epop_raceth, aes(x = white, y = black, color = year)) +
   geom_point()
 
-ggplot(filter(unemp_raceth, year > 1972), 
-       aes(x = month, y = bw_delta, color = periodName)) +
-  geom_path()
+ggplot(filter(epop_raceth, year > 1994), 
+       aes(x = month, y = bw_delta)) +
+  geom_point()
